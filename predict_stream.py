3834739c508c32de4116e686e5acc3131a7e9c5a
J <jeevikaravikumar0702@gmail.com>
2026-09-11 00:51:26 +0530

    predict_stream.py

--- predict_stream.py
+++ predict_stream.py
@@ -6,27 +6,19 @@ from alerts import send_critical_email
 
 print("🔌 Connecting to live Node.js WebSocket stream...")
 
-# Load our saved human-coded AI brain
 ai_brain = joblib.load('trained_machinery_model.pkl')
 
 def on_message(ws, message):
-    # This function triggers the exact millisecond a packet is broadcasted by Node!
     packet = json.loads(message)
-    
-    # Extract metrics
     packet_id = packet['id']
     temp = packet['temperature']
     vib = packet['vibration']
     speed = packet['rotational_speed']
     
-    # Human Data Cleaning: Handle the occasional None/Null value
     if vib is None:
         vib = 45.0
-        
-    # Format data for Scikit-Learn
     features = pd.DataFrame([[temp, vib, speed]], columns=['temperature', 'vibration', 'rotational_speed'])
     
-    # Run the prediction matrix
     failure_probability = ai_brain.predict_proba(features)[0][1]
     failure_percentage = failure_probability * 100
     
@@ -47,7 +39,6 @@ def on_error(ws, error):
 def on_close(ws, close_status_code, close_msg):
     print("🔌 Stream disconnected.")
 
-# Open connection link directly to the Node socket backend
 ws_url = "ws://localhost:8080"
 ws = websocket.WebSocketApp(ws_url,
                             on_message=on_message,
